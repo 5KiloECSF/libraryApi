37821c6183d17232dc211401399ae82b1d47af8d
bur <belay.bura@gmail.com>
2022-05-14 15:43:29 +0300

    made it work with new fb & atlas configrations

--- src/utils/firebase/firebaseAdmin.js
+++ src/utils/firebase/firebaseAdmin.js
@@ -1,12 +1,12 @@
 let admin = require("firebase-admin");
 const { v4: uuidv4 } = require('uuid');
 
-const serviceAccount = require("../../../env/aait-a3640-1.json");
+// const serviceAccount = require("../../../env/aait-a3640-1.json");
 const {fbConfig,FirebaseProjectName}=require('../../config/constants');
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    // credential: admin.credential.cert(fbConfig),
+    // credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert(fbConfig),
     databaseURL: `https://${FirebaseProjectName}.firebaseio.com`,
     storageBucket: `gs://${FirebaseProjectName}.appspot.com`
 });
